a03d75da8a048e800ef49bb4634f36072264ad3d
Tyson Tate <tyson@doloreslabs.com>
2011-01-27 16:25:39 -0800

    Revert "Add worker management API." Accidental push.
    This reverts commit 765b8ccc1a46f0e962e4d611487f0bf2ac0072fe.

--- lib/crowdflower.rb
+++ lib/crowdflower.rb
@@ -1,5 +1,5 @@
 require 'httparty'
 
-%w{base job unit judgment order worker}.each do |file|
+%w{base job unit judgment order}.each do |file|
   require File.dirname(__FILE__) +"/crowdflower/" + file
 end
