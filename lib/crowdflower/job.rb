efe314443301a9e3f129c27fcd96a8cfee92aff4
Casey Speer <casey@crowdflower.com>
2011-08-30 18:10:23 -0700

    accept a 302 response in csv download

--- lib/crowdflower/job.rb
+++ lib/crowdflower/job.rb
@@ -21,15 +21,15 @@ module CrowdFlower
       connect
       get(resource_uri)
     end
-    
+
     def self.upload(file, content_type, job = nil, opts = {})
       connect
-      
+
       job_uri = job ? "/#{job.kind_of?(Job) ? job.id : job}" : ""
       conn = job.kind_of?(Job) ? job.connection : self.connection
       upload_uri = "#{resource_uri}/#{job_uri}/upload".squeeze("/")
-      res = conn.post(upload_uri, 
-        :body => File.read(file), 
+      res = conn.post(upload_uri,
+        :body => File.read(file),
         :headers => {"content-type" => content_type},
         :query => opts)
 
@@ -44,7 +44,7 @@ module CrowdFlower
       verify_response res
       self.new(res["id"], nil, res)
     end
- 
+
     def get(id = nil)
       connection.get("#{resource_uri}/#{@id || id}")
     end
@@ -58,7 +58,7 @@ module CrowdFlower
     def units
       Unit.new(self)
     end
-    
+
     # Copies a job and optionally gold or all units.
     # Parameters
     #  opts: Hash
@@ -69,25 +69,25 @@ module CrowdFlower
       self.class.verify_response res
       self.class.new(res["id"], nil, res)
     end
-    
+
     def status
       connection.get("#{resource_uri}/#{@id}/ping")
     end
-    
+
     def upload(file, content_type)
       self.class.upload(file, content_type, self)
     end
-    
+
     def legend
       connection.get("#{resource_uri}/#{@id}/legend")
     end
-    
+
     def download_csv(type = :full, filename = nil)
       filename ||= "#{type.to_s[0].chr}#{@id}.csv"
       res = connection.get("#{resource_uri}/#{@id}.csv", {:format => :csv, :query => {:type => type}})
       self.class.verify_response res
       puts "Status... #{res.code.inspect}"
-      if res.code == 202
+      if res.code == 202 || res.code == 302
         puts "CSV Generating... Trying again in 10 seconds."
         Kernel.sleep 10
         download_csv(type, filename)
@@ -96,15 +96,15 @@ module CrowdFlower
         File.open(filename, "w") {|f| f.puts res.body }
       end
     end
-    
+
     def pause
       connection.get("#{resource_uri}/#{@id}/pause")
     end
-    
+
     def resume
       connection.get("#{resource_uri}/#{@id}/resume")
     end
-    
+
     def cancel
       connection.get("#{resource_uri}/#{@id}/cancel")
     end
@@ -114,33 +114,33 @@ module CrowdFlower
       self.class.verify_response(response)
       response
     end
-    
+
     def delete
       connection.delete("#{resource_uri}/#{@id}.json")
     end
-    
+
     def channels
       connection.get("#{resource_uri}/#{@id}/channels")
     end
-    
+
     def enable_channels(channels)
       connection.post("#{resource_uri}/#{@id}/channels", {:body => { :channels => channels } } )
     end
 
     def tags
-      connection.get("#{resource_uri}/#{@id}/tags") 
+      connection.get("#{resource_uri}/#{@id}/tags")
     end
 
     def update_tags(tags)
-      connection.put("#{resource_uri}/#{@id}/tags", {:body => { :tags => tags } } ) 
+      connection.put("#{resource_uri}/#{@id}/tags", {:body => { :tags => tags } } )
     end
 
     def add_tags(tags)
-      connection.post("#{resource_uri}/#{@id}/tags", {:body => { :tags => tags } } ) 
+      connection.post("#{resource_uri}/#{@id}/tags", {:body => { :tags => tags } } )
     end
 
     def remove_tags(tags)
-      connection.delete("#{resource_uri}/#{@id}/tags", {:body => { :tags => tags } } ) 
+      connection.delete("#{resource_uri}/#{@id}/tags", {:body => { :tags => tags } } )
     end
   end
 end
